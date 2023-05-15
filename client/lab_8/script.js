da774475f4430053585a0d1e8b37b582f23b57b7
Yamlak Shimelis <yshim1@terpmail.umd.edu>
2023-05-15 12:41:20 -0400

    final lab 8 commits

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -34,18 +34,45 @@ function getRandomIntInclusive(min, max){
     })
   }
   
+  function initMap(){
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(carto);
+  return carto;
+  }
+
+  function markerPlace(array, map){
+    console.log('array for markers', array);
+    map.eachLayer((layer) => {
+  if (layer instanceof L.Marker) {
+    layer.remove();
+  }
+});
+    array.forEach((item) => {
+      console.log('markerPlace', item);
+      const{coordinates} = item.geocoded_column_1;
+
+      L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    })
+  }
+
   async function mainEvent() { // the async keyword means we can make API requests
     const form = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector("#data_clear");
     const generateListButton = document.querySelector('#generate');
     const loadAnimation = document.querySelector('#data_load_animation');
     const textField = document.querySelector('#resto')
     loadAnimation.style.display = 'none'
     generateListButton.classList.add('hidden');
+
+    const carto = initMap();
     const storedData = localStorage.getItem('storedData');
-    const parsedData = JSON.parse(storedData);
+    let parsedData = JSON.parse(storedData);
     let currentList = [];
-    if(parsedData.length > 0){
+    if(parsedData?.length > 0){
       generateListButton.classList.remove("hidden");
     }
     loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
@@ -96,6 +123,10 @@ function getRandomIntInclusive(min, max){
       // This changes the response from the GET into data we can use - an "object"
       const storedList = await results.json();
       localStorage.setItem('storedData', JSON.stringify(storedList));
+      parsedData = storedList
+      if(parsedData?.length > 0){
+        generateListButton.classList.remove("hidden");
+      }
 
       loadAnimation.style.display = 'none'
       // console.table(storedList);
@@ -111,6 +142,7 @@ function getRandomIntInclusive(min, max){
       currentList = cutRestaurantList(parsedData);
       console.log(currentList);
       injectHTML(currentList);
+      markerPlace(currentList, carto);
     })
 
     textField.addEventListener('input', (event) => {
@@ -118,6 +150,13 @@ function getRandomIntInclusive(min, max){
         const newList = filterList(currentList, event.target.value);
         console.log(newList);
         injectHTML(newList)
+        markerPlace(newList, carto);
+    })
+
+    clearDataButton.addEventListener("click", (event) =>{
+      console.log('clear browser data');
+      localStorage.clear();
+      console.log('localStorage Check', localStorage.getItem("storedData"));
     })
   }
   
